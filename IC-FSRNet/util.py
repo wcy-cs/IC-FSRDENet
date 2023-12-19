3a5855b0a9f3882bffdce7464590c5a744e56077
wcy-cs <39185517+wcy-cs@users.noreply.github.com>
2023-12-19 21:10:52 +0800

    Update util.py

--- IC-FSRNet/util.py
+++ IC-FSRNet/util.py
@@ -1,8 +1,6 @@
 from skimage import measure
 import torch
 import numpy as np
-from skimage.metrics import peak_signal_noise_ratio
-from skimage.metrics import structural_similarity
 import math
 import cv2
 import os
@@ -12,58 +10,69 @@ def prepare(arg):
         arg = arg.cuda(0)
     return arg
 
-
-def cal_ssim(img1, img2):
+def calc_psnr(img1, img2):
+    # img1 and img2 have range [0, 255]
+    #
+    img1 = img1.astype(np.float64)
+    img2 = img2.astype(np.float64)
     img1_np = np.array(img1)
     img2_np = np.array(img2)
-    img1_np = img1_np.transpose(1, 2, 0)
-    img2_np = img2_np.transpose(1, 2, 0)
-    # print(img1_np)
-    return structural_similarity(img1_np, img2_np, data_range=1, multichannel=True)
+    mse = np.mean((img1_np - img2_np)**2)
+    if mse == 0:
+        return float('inf')
+    return 20 * math.log10(255.0 / math.sqrt(mse))
+def ssim(img1, img2):
+
+    C1 = (0.01 * 255)**2
+    C2 = (0.03 * 255)**2
+
+    # img1 = img1.astype(np.float64)
+    # img2 = img2.astype(np.float64)
+    img1_np = np.array(img1)
+    img2_np = np.array(img2)
+
+    kernel = cv2.getGaussianKernel(11, 1.5)
+    window = np.outer(kernel, kernel.transpose())
+
+    mu1 = cv2.filter2D(img1_np, -1, window)[5:-5, 5:-5]  # valid
+    mu2 = cv2.filter2D(img2_np, -1, window)[5:-5, 5:-5]
+    mu1_sq = mu1**2
+    mu2_sq = mu2**2
+    mu1_mu2 = mu1 * mu2
+    sigma1_sq = cv2.filter2D(img1_np**2, -1, window)[5:-5, 5:-5] - mu1_sq
+    sigma2_sq = cv2.filter2D(img2_np**2, -1, window)[5:-5, 5:-5] - mu2_sq
+    sigma12 = cv2.filter2D(img1_np * img2_np, -1, window)[5:-5, 5:-5] - mu1_mu2
+
+    ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) *
+                                                            (sigma1_sq + sigma2_sq + C2))
+    return ssim_map.mean()
 
 
+def calc_ssim(img1, img2):
 
-def convert_rgb_to_y(tensor):
-    image = tensor[0].cpu().numpy().transpose(1,2,0)#.detach()
+    '''calculate SSIM
+    the same outputs as MATLAB's
+    img1, img2: [0, 255]
+    '''
 
-    if len(image.shape) <= 2 or image.shape[2] == 1:
-        return image
+    if not img1.shape == img2.shape:
+        raise ValueError('Input images must have the same dimensions.')
+    # print(img1.shape)
+    if img1.ndim == 2:
+        return ssim(img1, img2)
+    elif img1.ndim == 3:
+        # print(img1.shape[2])
+        if img1.shape[2] == 3:
+            ssims = []
+            for i in range(3):
+                ssims.append(ssim(img1, img2))
+            return np.array(ssims).mean()
+        elif img1.shape[2] == 1:
+            return ssim(np.squeeze(img1), np.squeeze(img2))
+    else:
+        raise ValueError('Wrong input image dimensions.')
 
-    #xform = np.array([[65.481, 128.553, 24.966]])
-    #y_image = image.dot(xform.T) + 16.0
 
-    xform = np.array([[65.738 / 256.0, 129.057 / 256.0, 25.064 / 256.0]])
-    y_image = image.dot(xform.T) + 16.0
-
-    return y_image
-
-
-def calc_psnr(sr, hr, scale, rgb_range, dataset=None, facebb=[]):
-    # Y channel
-    if hr.nelement() == 1: return 0
-
-    diff = (sr - hr) / rgb_range
-    shave = scale
-    facebb = facebb[0].numpy()
-    if diff.size(1) > 1:
-        gray_coeffs = [65.738, 129.057, 25.064]
-        convert = diff.new_tensor(gray_coeffs).view(1, 3, 1, 1) / 256
-        diff = diff.mul(convert).sum(dim=1)
-
-    _, _, w, h = hr.size()
-    x1 = max(int(facebb[0]), shave)
-    x2 = min(int(facebb[2]), w-shave)
-    y1 = max(int(facebb[1]), shave)
-    y2 = min(int(facebb[3]), h-shave)
-
-    image1 = convert_rgb_to_y(sr)
-    image2 = convert_rgb_to_y(hr)
-    image1 = image1[y1:y2, x1:x2, :]
-    image2 = image2[y1:y2, x1:x2, :]
-    psnr = peak_signal_noise_ratio(image1, image2, data_range=rgb_range)
-    ssim = structural_similarity(image1, image2, win_size=11, gaussian_weights=True, multichannel=True, K1=0.01, K2=0.03,
-                        sigma=1.5, data_range=rgb_range)
-    return psnr, ssim
 
 def rgb2ycbcr(img, only_y=True):
     in_img_type = img.dtype
